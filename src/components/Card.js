ad8f0f04201249378883171396c19e1914e9157c
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-11-17 21:03:55 +1100

    added ui styling and images for hog cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Card({ hogInfo }) {
   return (
-    <div>
-        <h3>{hogInfo.name}</h3>
+    <div className="ui four wide column">
+        <div className="ui card pigTile">
+            <h3>{hogInfo.name}</h3>
+            <img src={hogInfo.image}></img>
+        </div>
     </div>
   )
 }
--- src/components/CardDisplay.js
+++ src/components/CardDisplay.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Card from './Card'
 
-export default function CardDisplay({hogs}) {
+export default function CardDisplay({ hogs }) {
   
     const hogsForDisplay = hogs.map((hog) => {
-        return <Card key={hog.id} hogInfo={hog} />
+        return <Card key={hog.id} hogInfo={hog}/>
     })
 
     return (
     <div>
         <h2>Hogs</h2>
+        <div className="ui grid container">
         {hogsForDisplay}
+        </div>
     </div>
   )
 }
