1e07eb6edbc3e621f901ded292784e9ca4dcca35
Michelle Di Guglielmo <mfdigug@gmail.com>
2025-11-17 20:20:26 +1100

    added components Filter, Sort, CardDisplay, Card

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Nav from "./Nav";
+import Filter from "./Filter";
+import Sort from "./Sort";
+import CardDisplay from "./CardDisplay";
 
 import hogs from "../porkers_data";
 
@@ -7,6 +10,9 @@ function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<Filter />
+			<Sort />
+			<CardDisplay />
 		</div>
 	);
 }
